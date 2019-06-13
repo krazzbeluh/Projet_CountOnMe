bce871983c849e937fe86a2ad9428cdfcec9f27b
Paul Leclerc <paulleclerc@icloud.com>
2019-06-13 12:40:58 +0200

    Added UI Tests

--- CountOnMeUITests/CountOnMeUITests.swift
+++ CountOnMeUITests/CountOnMeUITests.swift
@@ -36,9 +36,51 @@ class CountOnMeUITests: XCTestCase {
     func testGivenTextViewIs1Plus1Equal2_WhenTapping6_ThenTextViewIs6() {
         XCTAssert(screenText == "1+1=2")
         
-        XCUIApplication().buttons["6"].tap()
+        app.buttons["6"].tap()
         
         XCTAssert(screenText == "6")
+    }
+    
+    func testGivenTextViewIsDefault_WhenTapping12Times3Equal_ThenTextViewIs12Times3Equals36() {
+        
+        app.buttons["1"].tap()
+        app.buttons["2"].tap()
+        app.buttons["×"].tap()
+        app.buttons["3"].tap()
+        app.buttons["="].tap()
+        
+        XCTAssert(screenText == "12 × 3 = 36")
+    }
+    
+    func testGivenTextViewIsDefault_WhenTapping4Plus5Equal_ThenTextViewIs4Plus5Equals9() {
+        
+        app.buttons["4"].tap()
+        app.buttons["+"].tap()
+        app.buttons["5"].tap()
+        app.buttons["="].tap()
+        
+        XCTAssert(screenText == "4 + 5 = 9")
+    }
+    
+    func testGivenTextViewIsDefault_WhenTapping80Minus7Equal_ThenTextViewIs6Minus5Equals73() {
+        
+        app.buttons["8"].tap()
+        app.buttons["0"].tap()
+        app.buttons["-"].tap()
+        app.buttons["7"].tap()
+        app.buttons["="].tap()
+        
+        XCTAssert(screenText == "80 - 7 = 73")
+    }
+    
+    func testGivenTextViewIsDefault_WhenTapping9DividedBy6Equal_ThenTextViewIs9DividedBy6Equals1Dot5() {
+        
+        app.buttons["9"].tap()
+        app.buttons["÷"].tap()
+        app.buttons["6"].tap()
+        app.buttons["="].tap()
+        
+        XCTAssert(screenText == "9 ÷ 6 = 1.5")
     }
     
     override func tearDown() {
